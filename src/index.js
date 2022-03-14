8761c79ec03cfc6cc34f6c44b8f285112b606912
Adam Luzsi <luzsiadam@gmail.com>
2022-03-14 17:51:25 +0100

    + Block icon declaration changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import Edit from './edit';
 import save from './save';
 
 registerBlockType('blocks-course/text-box', {
+	/* icon: <svg version="1.1" viewBox="0 0 500 500" preserveAspectRatio='xMinYMin meet'>
+			<circle cx="250" cy="250" r="200" />
+		</svg>, */
+	icon: {
+		src: 'text-page',
+		/* src: (<svg version="1.1" viewBox="0 0 500 500" preserveAspectRatio='xMinYMin meet'>
+				<circle cx="250" cy="250" r="200" />
+			</svg>), */
+		foreground: '#c1001b',
+	},
 	edit: Edit,
 	save,
 });
