ba7c4463e7fb54de06af4cd8f9552d244954fa54
Adam Luzsi <luzsiadam@gmail.com>
2022-03-14 17:44:46 +0100

    + UPdated build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '901e1ec9ed01523c9b40f10565314b09');
